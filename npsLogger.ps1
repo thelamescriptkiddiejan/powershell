84b818a1332ae8b382a6097560cf9b6336fbdc69
thelamescriptkiddiejan <jbexten@protonmail.com>
2020-04-15 15:22:55 +0200

    Final

--- npsLogger.ps1
+++ npsLogger.ps1
@@ -1,33 +1,29 @@
 $timetag = (Get-Date -Format "dd/MM/yyyy")
 $csvName =  [System.String]::Concat("NPS-Log_", $timetag, ".csv")
+
+#-----------Diese Variablen anpassen-----------
 $csvDrive = "C:"
 $csvPath = "Users\Public"
 $logordner = "meinelogs"
+
+#-----------Diese Variablen in Ruhe lassen-----------
 $csvcombinepath = ("$csvDrive\$csvPath")
 $csvFULLpfad = ("$csvDrive\$csvPath\$logordner\$csvName")
 $checkpath = ("$csvDrive\$csvPath\$logordner")
-
 $infostring = [System.String]::Concat("`n", "     ", $logordner, " NICHT vorhanden!", "`n", "   Erstelle ", $csvFULLpfad, "!")
-#-------------------------------------------------------------------------
-
-
-
-
+ 
+#-----------Teste ob der Pfad vorhanden ist-------------------
 If(!(test-path $checkpath))
 {
-    
+#-----------Pfad vorhanden------------------------------------
     Write-Host $infostring
     
     New-Item -Path $csvcombinepath -Name $logordner -ItemType "directory"
-    
     Get-EventLog -LogName System -After (get-date).addminutes(-60) -EntryType error -Source "NPS" | Export-CSV $csvFULLpfad
-    Start-Sleep -Seconds 1.5
 }
 else {
-
-    Write-Host "   Pfad VORAHNDEN!!!1!!!111"
-
-    Start-Sleep -Seconds 1.5
+#-----------Pfad nicht vorhanden--------------------------------
+    Write-Host "Der Pfad ist vorhanden und die Datei", $csvname, "wurde dort abgelegt."
     Get-EventLog -LogName System -After (get-date).addminutes(-60) -EntryType error -Source "NPS" | Export-CSV $csvFULLpfad -append 
 }
 #-------------------------------------------------------------------------

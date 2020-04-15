81e0c7c33eb74de00e132dc7c17cd99fb1dad08f
thelamescriptkiddiejan <jbexten@protonmail.com>
2020-04-15 14:54:01 +0200

    1.0

--- npsLogger.ps1
+++ npsLogger.ps1
@@ -1,17 +1,32 @@
 $timetag = (Get-Date -Format "dd/MM/yyyy")
-$csvname =  [System.String]::Concat("NPS-Log_", $timetag, ".csv")
-$csvDrive = "C:\"
-$csvPath = "meinelogs"
+$csvName =  [System.String]::Concat("NPS-Log_", $timetag, ".csv")
+$csvDrive = "C:"
+$csvPath = "Users\Public\meinelogs"
+$csvFULLpfad = ("$csvDrive\$csvPath\$csvName")
+$checkpath = ("$csvDrive\$csvPath")
+ 
+#--------------------------------------------------------
 
 
-If(!(test-path $csvCHECKpfad))
+
+
+
+If(!(test-path $checkpath))
 {
     New-Item -Path $csvDrive -Name $csvPath -ItemType "directory"
     
-Get-EventLog -LogName System -After (get-date).addminutes(-60) -EntryType error | Export-CSV $csvname
+    Get-EventLog -LogName System -After (get-date).addminutes(-60) -EntryType error | Export-CSV $csvFULLpfad
     Start-Sleep -Seconds 1.5
 }
+else {
+    Start-Sleep -Seconds 1.5
+    Get-EventLog -LogName System -After (get-date).addminutes(-60) -EntryType error | Export-CSV $csvFULLpfad -append 
+}
+#-------------------------------------------------------------------------
 
-Start-Sleep -Seconds 1.5
 
-Get-EventLog -LogName System -After (get-date).addminutes(-60) -EntryType error | Export-CSV C:\logpath.csv -append
+
+
+
+
+
